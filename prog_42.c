3aa48fb8b95a87ec26ad4e85ab5b5f24a9c00c5b
neelaryan <neeldagr8hacker@gmail.com>
2014-05-16 11:51:01 +0530

    Final Commit. TC console display will go out of screen if strlen > 25.

---
+++ prog_42.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+/* If we are compiling on Windows compile these functions and/or add these headers */
+#if defined(_WIN16)
+
+#include <conio.h>
+/* Clear the screen using this function when conio.h is not present */
+void clrscr()
+{
+    printf("\x1B[2J\x1B[0;0f");
+}
+
+/* Otherwise define getch() and getche() */
+#elif defined(__linux__)
+
+#include <termios.h>
+static struct termios old, new;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+    tcgetattr(0, &old); /* grab old terminal i/o settings */
+    new = old; /* make new settings same as old settings */
+    new.c_lflag &= ~ICANON; /* disable buffered i/o */
+    new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+    tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+    tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+    char ch;
+    initTermios(echo);
+    ch = getchar();
+    resetTermios();
+    return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+    return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+    return getch_(1);
+}
+
+/* Clear the screen */
+void clrscr()
+{
+    printf("\x1B[2J\x1B[0;0f");
+}
+
+#endif
+
+int main()
+{
+    char str[80];
+    int i,j;
+    clrscr();
+    printf("Enter string : ");
+    fgets(str,80,stdin);
+    if((strlen(str)>0) && (str[strlen(str)-1] == '\n'))
+        str[strlen(str)-1]='\0';
+    printf("Output :\n");
+    for(i=0; str[i]!='\0'; i++)
+    {
+        printf("\n");
+        for(j=0; j<i; j++)
+            printf(" ");
+        printf("%c",str[i]);
+    }
+    getch();
+    return 0;
+}
