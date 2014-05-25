fcfa6deaca543d253cdeb35bc333003ce107fef0
neelaryan <neeldagr8hacker@gmail.com>
2014-05-25 13:41:20 +0530

    scanf() buffer underflow

---
+++ prog_68.c
@@ -0,0 +1,193 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+/* If we are compiling on Windows compile these functions and/or add these headers */
+#if defined(_WIN16)
+
+#include <conio.h>
+/* Clear the screen using this function when conio.h is not present */
+void clrscr()
+{
+    printf("\x1B[2J\x1B[0;0f");
+}
+
+/* Otherwise define getch() and getche() */
+#elif defined(__linux__)
+
+#include <termios.h>
+static struct termios old, new;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+    tcgetattr(0, &old); /* grab old terminal i/o settings */
+    new = old; /* make new settings same as old settings */
+    new.c_lflag &= ~ICANON; /* disable buffered i/o */
+    new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+    tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+    tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+    char ch;
+    initTermios(echo);
+    ch = getchar();
+    resetTermios();
+    return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+    return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+    return getch_(1);
+}
+
+/* Clear the screen */
+void clrscr()
+{
+    printf("\x1B[2J\x1B[0;0f");
+}
+
+#endif
+
+typedef struct tree
+{
+    char data;
+    struct tree *left;
+    struct tree *right;
+} tree_t;
+
+tree_t *getnode(char x)
+{
+    tree_t *p;
+    p = (tree_t *)malloc(sizeof(tree_t));
+    p->left = NULL;
+    p->right = NULL;
+    p->data = x;
+    return p;
+}
+
+tree_t *createBST()
+{
+    tree_t *root, *p, *ptr;
+    char a;
+    int flag=-1,s,reads;
+    printf("Enter root element (0 to stop) : ");
+    while(((reads = scanf("%d%c", &s, &a)) != 2 && reads != EOF) || (a != '\n') || !isalpha(s)) {
+        printf("Please enter a character only !\nEnter root element (0 to stop) : ");
+        do {
+            reads = scanf("%d%c", &s,&a);
+        }while((reads != EOF || a != '\n') || !isalpha(s));
+    }
+    root = getnode(s);
+    flag=-1;
+    printf("Enter another element (0 to stop) : ");
+    while(((reads = scanf("%d%c", &s, &a)) != 2 && reads != EOF) || a != '\n') {
+        printf("Please enter a character only !\nEnter root element (0 to stop) : ");
+        do {
+            reads = scanf("%c", &a);
+        }while(reads != EOF && a != '0');
+    }
+    do
+    {
+        ptr = root;
+        p = root;
+        while(p != NULL)
+        {
+            ptr = p;
+            if(s < p->data)
+                p = p->left;
+            else
+                p = p->right;
+        }
+        if(s < ptr->data)
+            ptr->left = getnode(s);
+        else
+            ptr->right = getnode(s);
+        flag=-1;
+        do
+        {
+            flag++;
+            if(flag)
+                printf("Please enter an character only !\nEnter an element (0 to stop) : ");
+            else
+                printf("Enter an element (0 to stop) : ");
+            s = getchar();
+            getchar();
+            if(s == '0')
+                return root;
+        }while(!isalpha(s));
+    }while(s != '0');
+    return root;
+}
+
+void preorder(tree_t *root)
+{
+    static int flag = 0;
+    if(root != NULL)
+    {
+        flag++;
+        printf("%c ",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+    else if(flag == 0)
+        printf("NULL");
+}
+
+void inorder(tree_t *root)
+{
+    static int flag = 0;
+    if(root != NULL)
+    {
+        flag++;
+        inorder(root->left);
+        printf("%c ",root->data);
+        inorder(root->right);
+    }
+    else if(flag == 0)
+        printf("NULL");
+}
+
+void postorder(tree_t * root)
+{
+    static int flag = 0;
+    if(root != NULL)
+    {
+        flag++;
+        postorder(root->left);
+        postorder(root->right);
+        printf("%c ",root->data);
+    }
+    else if(flag == 0)
+        printf("NULL");
+}
+
+int main()
+{
+    clrscr();
+    tree_t *myTree;
+    printf("Building BST...\n\n");
+    myTree = createBST();
+    printf("\n\nPreorder Traversal :\n");
+    preorder(myTree);
+    printf("\n\nInorder Traversal :\n");
+    inorder(myTree);
+    printf("\n\nPostorder Traversal :\n");
+    postorder(myTree);
+    getch();
+    return 0;
+}
